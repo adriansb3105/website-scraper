4dc19fb5d608f9789f48ac2a6985ed875868429d
Adrian Serrano <adriansb3105@gmail.com>
2024-02-17 15:37:44 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+// @ts-check
+
+const { chromium } = require('playwright')
+
+const shops = [
+    {
+        vendor: 'Microsoft',
+        url: 'https://www.xbox.com/es-es/configure/8WJ714N3RBTL',
+        checkStock: async ({ page }) => {
+            const content = await page.textContent('[aria-label="Finalizar la compra del pack"]')
+            return content?.includes('Finalizar la compra')
+        }
+    },
+    {
+        vendor: 'Soporte Consolas CR',
+        url: 'https://soporteconsolascr.com/products/xbox-series-x-1tb',
+        checkStock: async ({ page }) => {
+            const content = await page.textContent('#AddToCartText-product-template')
+            return content.includes('Agregar al carrito')
+        }
+    }
+]
+
+;(async () => {
+    const browser = await chromium.launch({ headless: false })
+
+    for(const shop of shops) {
+        const { checkStock, vendor, url } = shop
+        
+        const page = await browser.newPage()
+        await page.goto(url)
+        const hasStock = await checkStock({ page })
+        
+        console.log(`${vendor}: ${hasStock ? 'in stock' : 'out of stock'}`)
+
+        await page.close()
+    }
+
+    await browser.close()
+})()

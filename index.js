068c8b12c7adbefe207becfbdc921481d25eb70b
Adrian Serrano <adriansb3105@gmail.com>
2024-02-19 12:03:34 -0600

    Remove the take of screens, and removed the carets in packaeg.json

--- index.js
+++ index.js
@@ -18,7 +18,32 @@ const shops = [
             const content = await page.textContent('#AddToCartText-product-template')
             return content.includes('Agregar al carrito')
         }
+    },
+    {
+        vendor: 'El Corte Ingles',
+        url: 'https://www.elcorteingles.es/videojuegos/A37047078-xbox-series-x',
+        checkStock: async ({ page }) => {
+            const content = await page.textContent('.product_detail-add_to_cart')
+            return content.includes('Añadir a la cesta')
+        }
     }
+    /*{
+        vendor: 'Fnac',
+        url: 'https://www.fnac.es/Consola-Xbox-Series-X-Diablo-IV-Videoconsola-Consola/a10276544',
+        checkStock: async ({ page }) => {
+            const content = await page.textContent('[data-automation-id="product-buy-btn"]')
+            return content.includes('Añadir a la cesta')
+        }
+    },
+    {
+        vendor: 'Amazon',
+        url: 'https://www.amazon.com/-/es/Xbox-Forza-Horizons-Console-Bundle-X/dp/B0BNWDC18R/ref=sr_1_3',
+        checkStock: async ({ page }) => {
+            const addToCartButton = await page.$$('#add-to-cart-button') //Return an array
+            console.log(addToCartButton);
+            return addToCartButton.length > 0
+        }
+    }*/ //Disabled for now cause is not working properly
 ]
 
 ;(async () => {
@@ -31,8 +56,8 @@ const shops = [
         await page.goto(url)
         const hasStock = await checkStock({ page })
         
-        console.log(`${vendor}: ${hasStock ? 'in stock' : 'out of stock'}`)
-        await page.screenshot({ path: `screenshots/${vendor}.png` })
+        console.log(`${vendor}: ${hasStock ? 'HAS STOCK!!!' : 'Out of Stock'}`)
+        //await page.screenshot({ path: `screenshots/${vendor}.png` })
 
         await page.close()
     }
